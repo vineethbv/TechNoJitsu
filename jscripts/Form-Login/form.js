fb062cb29186f88e9645f62e249cc5d39e8d866d
AjithKumarG <53942428+Ajith-Kumar-G@users.noreply.github.com>
2020-12-16 09:21:33 +0530

    validation

--- jscripts/Form-Login/form.js
+++ jscripts/Form-Login/form.js
@@ -2,3 +2,17 @@ var particles = Particles.init({
 	selector: '.background',
   color: '#DA0463'
 });
+
+
+function check(){
+  var un = document.getElementsByClassName("name");
+  var pw = document.getElementsByClassName("password");
+  var username = "Admin"; 
+  var password = "AdminOfPage*123";
+  if ((un === username) && (pw === password)) {
+    location.replace= "../../TechNoJitsu.html";
+  }
+  else {
+      alert ("Login was Unsuccessful, please check your username and password");
+  }
+}
